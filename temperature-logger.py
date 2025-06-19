1214ffbb7fef5948d11fb7ca3f60e75447008f1e
David Frankland <david@dmf-software.co.uk>
2025-06-19 16:43:22 +0100

    add MQTT

--- temperature-logger.py
+++ temperature-logger.py
@@ -3,6 +3,8 @@ import datetime
 import logging
 import logging.config
 import serial
+import paho.mqtt.publish
+
 
 # setup logging
 logging.config.fileConfig("logging.ini")
@@ -18,6 +20,10 @@ port = settings.get("port", '/dev/ttyUSB0')
 logger.debug(f"port = {port}")
 interval = settings.getint("interval", 60)
 logger.debug(f"interval = {interval}")
+mqtt_host = settings.get("mqtt-host")
+logger.debug(f"mqtt_host = {mqtt_host}")
+mqtt_topic = settings.get("mqtt-topic")
+logger.debug(f"mqtt_topic = {mqtt_topic}")
 
 serial = serial.Serial(port)
 serial.reset_input_buffer()
@@ -33,6 +39,11 @@ while True:
         str = line.decode()
         temperature = float(str)
         logger.info(temperature)
+        try:
+            logger.debug(f"sending {mqtt_topic} to {mqtt_host}")
+            paho.mqtt.publish.single(mqtt_topic, temperature, hostname=mqtt_host)
+        except:
+            logger.debug(f'failed to publish {mqtt_topic} message')
     except:
         logger.debug(f'invalid temperature: {str}')
     time_to_read = datetime.datetime.now() + datetime.timedelta(seconds=interval)
