1e36d0ef066678cdd1fb2da66ae0be3624b9d30b
David Frankland <david@dmf-software.co.uk>
2025-10-16 23:26:56 +0100

    remove CSV logging

--- temperature-logger.py
+++ temperature-logger.py
@@ -9,41 +9,40 @@ import paho.mqtt.publish
 # setup logging
 logging.config.fileConfig("logging.ini")
 logger = logging.getLogger()
-logger.debug("starting up")
+logger.info("starting up")
 
 # read settings from config file
-logger.debug("reading config settings")
+logger.info("reading config settings")
 config = configparser.ConfigParser()
 config.read("settings.ini")
 settings = config["settings"]
 port = settings.get("port", '/dev/ttyUSB0')
-logger.debug(f"port = {port}")
+logger.info(f"port = {port}")
 interval = settings.getint("interval", 60)
-logger.debug(f"interval = {interval}")
-mqtt_host = settings.get("mqtt-host")
-logger.debug(f"mqtt_host = {mqtt_host}")
-mqtt_topic = settings.get("mqtt-topic")
-logger.debug(f"mqtt_topic = {mqtt_topic}")
+logger.info(f"interval = {interval}")
+mqtt_host = settings.get("mqtt-host", "localhost")
+logger.info(f"mqtt_host = {mqtt_host}")
+mqtt_topic = settings.get("mqtt-topic", "temperature")
+logger.info(f"mqtt_topic = {mqtt_topic}")
 
 serial = serial.Serial(port)
 serial.reset_input_buffer()
 
-line = ''
-str = ''
+line: bytes
 time_to_read = datetime.datetime.now() + datetime.timedelta(seconds=1)
 while True:
     logger.debug(f'waiting until {time_to_read}')
     while datetime.datetime.now() < time_to_read:
         line = serial.readline()
     try:
-        str = line.decode()
-        temperature = float(str)
-        logger.info(temperature)
+        line_str = line.decode()
+        temperature = float(line_str)
+        logger.debug(f'temperature is {temperature}')
         try:
             logger.debug(f"sending {mqtt_topic} to {mqtt_host}")
             paho.mqtt.publish.single(mqtt_topic, temperature, hostname=mqtt_host)
-        except:
-            logger.debug(f'failed to publish {mqtt_topic} message')
+        except Exception as ex:
+            logger.error(f'failed to publish {mqtt_topic} message: {ex}')
     except:
-        logger.debug(f'invalid temperature: {str}')
+        logger.error(f'invalid temperature: {line_str}')
     time_to_read = datetime.datetime.now() + datetime.timedelta(seconds=interval)
